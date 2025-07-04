4d64a605bea850dd8edcfb3e4b133f23e1e06574
robercast <dunamis1.16@gmail.com>
2025-07-03 19:52:54 -0700

    initial commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// !!  importante
+// No utilices imágenes que no son del dominio público
+// Por ejemplo itera sobre las imágenes y checa si es del dominio público, asíÑ
+/*        if (artwork.is_public_domain === true) {          */
+// 3       SAFE to use: Display the image in your gallery
+// 4       displayImage(artwork.image_url);
+// 5     } else {
+// 6       // NOT SAFE: Do not use the image.
+// 7       // You can either skip this artwork entirely or display only the
+// 8       // text information (title, artist) without the image.
+// 9       console.log("Skipping copyrighted work:", artwork.title);
